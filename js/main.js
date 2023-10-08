3bed77f6d86fe32a7dd0246eb73d0efb4f7538f2
Adam Sjögren <adam.ch.sjogren@gmail.com>
2023-10-08 16:38:06 +0200

    Lagt till variabler för de olika ID jag kommer behöva använda.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 /*
 Här lägger du din JavaScript-kod
 */
+"use strict";
+
+// Variables
+let textBar = document.getElementById("newtodo");
+let newBtnEl = document.getElementById("newtodobutton");
+let newSpanEl = document.getElementById("message");
+let toDoListEl = document.getElementById("todolist");
+let clearBtnEl = document.getElementById("clearbutton");
+let i;
+
+//Event listeners
+window.onload = init;
+
+//Functions
+function addCourse(){
+    //Add courses
+}
+
+function clearCourse(){
+    //Remove courses
+}
